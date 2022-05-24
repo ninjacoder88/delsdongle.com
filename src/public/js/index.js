e14fa1c81330de0aa24478501f62bd28e2f243e8
Tom Harris <tomharris88@outlook.com>
2022-05-24 18:52:50 -0500

    added live twitch check

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -4,8 +4,27 @@ jQuery(function(){
     function ViewModel(){
         const self = this;
         self.redirectTime = ko.observable(20);
+        self.isLiveOnTwitch = ko.observable(false);
+        self.iframeSrc = ko.observable("");
 
-       
+        function checkStatus() {
+            $.ajax({
+                method: "GET",
+                url: "/twitch"
+            }).done((r) => {
+                console.log(r);
+                const obj = JSON.parse(r);
+                if(obj === true){
+                    self.iframeSrc("https://player.twitch.tv/?channel=delirya&parent=delsdongle.com");
+                    self.isLiveOnTwitch(true);
+                    return;
+                }
+            }).fail((j,t,e) => {
+                console.error(j);
+            });
+        }
+
+        checkStatus();
         //https://www.youtube.com/watch?v=VOreEIOVnCI
     }
 
--- src/server.js
+++ src/server.js
@@ -1,9 +1,43 @@
 const express = require("express");
+const https = require("https");
 
 const app = express();
 const port = 80;
 
 app.use(express.static("public"));
+
+app.get("/twitch", (request, response) => {
+
+    const options = {
+        hostname: "api.twitch.tv",
+        port: 443,
+        path: "helix/streams?user_login=delirya",
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${process.env.TwitchAuthToken}`,
+            "Client-Id": `${process.env.TwitchClientId}`
+        }
+    };
+
+    const httpsResponse = https.request(options, httpsResponse => {
+        httpsResponse.on("data", d => {
+            const twitchApiResponse = JSON.parse(d);
+            const data = twitchApiResponse.data;
+            if(data[0]){
+                response.send("true");
+                return;
+            }
+            response.send("false");
+        });
+
+        httpsResponse.on("error", error => {
+            response.send("false");
+        });
+    });
+
+    httpsResponse.end();
+});
 
 app.listen(port, () => {
     console.log(`app listening on port ${port}`);
